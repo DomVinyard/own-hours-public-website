d0601f82c26e2d5dee6318deee1cc5b2f669e3af
Dom Vin <i@dom.vin>
2023-01-07 14:53:21 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Own Hours</h1>
+      <h2>Download on app store</h2>
+    </div>
+  );
+}
